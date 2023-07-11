60debfac95939f0287ab40f8649133d3d46bf295
thib146 <thibaut@jumboprivacy.com>
2023-07-11 18:41:51 +0200

    create shoe list fragment layout + add logout menu

--- app/src/main/java/com/udacity/shoestore/home/ShoeListFragment.kt
+++ app/src/main/java/com/udacity/shoestore/home/ShoeListFragment.kt
@@ -1,4 +1,43 @@
 package com.udacity.shoestore.home
 
-class ShoeListFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.view.MenuHost
+import androidx.core.view.MenuProvider
+import androidx.fragment.app.Fragment
+import com.udacity.shoestore.R
+
+class ShoeListFragment: Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        val menuHost: MenuHost = requireActivity()
+        menuHost.addMenuProvider(object: MenuProvider {
+            override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+                menuInflater.inflate(R.menu.shoelist_menu, menu)
+            }
+
+            override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+                return when (menuItem.itemId) {
+                    R.id.menu_item_logout -> {
+                        // TODO: add viewModel method call
+                        true
+                    }
+                    else -> false
+                }
+            }
+        })
+
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+
 }
