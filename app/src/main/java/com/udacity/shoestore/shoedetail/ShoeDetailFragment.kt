0718e879952f2eaa2179cb53af677f1e1d6b9b2b
thib146 <thibaut@jumboprivacy.com>
2023-07-18 14:46:51 +0200

    add shoe saving logic (using input fields) + error behavior

--- app/src/main/java/com/udacity/shoestore/shoedetail/ShoeDetailFragment.kt
+++ app/src/main/java/com/udacity/shoestore/shoedetail/ShoeDetailFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
@@ -13,10 +14,13 @@ import com.udacity.shoestore.R
 import com.udacity.shoestore.databinding.FragmentShoeDetailsBinding
 import com.udacity.shoestore.home.ShoeListFragmentArgs
 import com.udacity.shoestore.models.Shoe
+import java.lang.NumberFormatException
 
 class ShoeDetailFragment: Fragment() {
 
     private lateinit var viewModel: ShoeDetailViewModel
+
+    private lateinit var binding: FragmentShoeDetailsBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -24,7 +28,7 @@ class ShoeDetailFragment: Fragment() {
         savedInstanceState: Bundle?
     ): View? {
 
-        val binding: FragmentShoeDetailsBinding = DataBindingUtil.inflate(
+        binding = DataBindingUtil.inflate(
             inflater,
             R.layout.fragment_shoe_details,
             container,
@@ -32,9 +36,6 @@ class ShoeDetailFragment: Fragment() {
         )
 
         viewModel = ViewModelProvider(this)[ShoeDetailViewModel::class.java]
-
-        // TODO: remove this line and get data from Input fields
-        val newShoe = Shoe("TestName" + Math.random().toString(), 45.0, "TestCompany", "")
 
         // Get args using by navArgs property delegate
         val shoeListFragmentArgs by navArgs<ShoeListFragmentArgs>()
@@ -47,11 +48,31 @@ class ShoeDetailFragment: Fragment() {
         }
 
         binding.shoeDetailSaveButton.setOnClickListener {
-            var latestShoeList: Array<Shoe> = viewModel.shoeList.value?.toTypedArray() ?: arrayOf()
-            latestShoeList = latestShoeList.plus(newShoe)
-            view?.findNavController()?.navigate(ShoeDetailFragmentDirections.actionShoeDetailFragmentToShoeListFragment(latestShoeList))
+            saveShoeAndNavigateToShoeListScreen()
         }
 
         return binding.root
+    }
+
+    private fun saveShoeAndNavigateToShoeListScreen() {
+        val shoeName = binding.shoeNameInput.text.toString()
+        val shoeCompany = binding.shoeCompanyInput.text.toString()
+        val shoeSizeString = binding.shoeSizeInput.text.toString()
+
+        if (shoeName.isEmpty() ||  shoeCompany.isEmpty() || shoeSizeString.isEmpty()) {
+            Toast.makeText(requireActivity(), getString(R.string.shoe_detail_fields_empty_error_message), Toast.LENGTH_LONG).show()
+        } else {
+            try {
+                val shoeSizeDouble = shoeSizeString.toDouble()
+                val newShoe = Shoe(shoeName, shoeSizeDouble, shoeCompany, "")
+                var latestShoeList: Array<Shoe> = viewModel.shoeList.value?.toTypedArray() ?: arrayOf()
+                latestShoeList = latestShoeList.plus(newShoe)
+
+                view?.findNavController()?.navigate(ShoeDetailFragmentDirections.actionShoeDetailFragmentToShoeListFragment(latestShoeList))
+            } catch (e: NumberFormatException) {
+                Toast.makeText(requireActivity(), getString(R.string.shoe_detail_size_field_wrong_format), Toast.LENGTH_LONG).show()
+            }
+
+        }
     }
 }
